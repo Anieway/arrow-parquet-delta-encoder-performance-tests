76b0da871232a43bea91b4970b14d7f92ab054e9
Anieway <daniel.vitzthum@tum.de>
2023-02-09 14:33:45 +0100

    Added 32 bit version of the test
    Added 32bit version of the test core later required for measuring lemire/FastCoding integration

--- src/EncoderRoundtripTest.h
+++ src/EncoderRoundtripTest.h
@@ -4,7 +4,17 @@
  *        parquet format with DELTA_BINARY_PACKED encoding
  * 
  * @param sample_repeat The number of times to repeat the measurement to account for disturbances
- * @param in_data The data to use for the roundtrip
+ * @param in_data The int64_t data to use for the roundtrip
  * @return A std::pair of encoding time and decoding time as int64_t in µs
  */
 std::pair<int64_t, int64_t> encoder_roundtrip(int sample_repeat, std::vector<int64_t> &in_data);
+
+/**
+ * @brief Takes a int32_t vector and measures the time it takes to encode and decode
+ *        parquet format with DELTA_BINARY_PACKED encoding
+ * 
+ * @param sample_repeat The number of times to repeat the measurement to account for disturbances
+ * @param in_data The int32_t data to use for the roundtrip
+ * @return A std::pair of encoding time and decoding time as int64_t in µs
+ */
+std::pair<int64_t, int64_t> encoder_roundtrip(int sample_repeat, std::vector<int32_t> &in_data);
